e143e5ad7707ad45f1d25609e18f88ca308f42a9
Agustin Diaz <Agustin Diaz@AgustinDiaz-PC>
2021-03-10 22:13:41 -0600

    Programa para calcular el error absoluto,relativo y relativo porcentual

--- src/ecuacionescuadraticas/EcuacionesCuadraticas.java
+++ src/ecuacionescuadraticas/EcuacionesCuadraticas.java
@@ -4,26 +4,115 @@ public class EcuacionesCuadraticas {
 
     public static void main(String[] args) {
        Scanner teclado = new Scanner(System.in);
-       int a,b,c,igual;
-        System.out.println("Ingresa el valor de a(x^2)");
-        a=teclado.nextInt();
-        System.out.println("Ingresa el valor de b(x)");
-        b=teclado.nextInt();
-        System.out.println("Ingresa el valor de c(termino independiente)");
-        c=teclado.nextInt();
-        System.out.println("Todo esto es igual a:");
-        igual=teclado.nextInt();
-        c=c-igual;
-        System.out.println("Resolucion por formula general:");
-        FormulaGeneral(a,b,c);
-        System.out.println("Resolucion por el metodo de Po-Shen Loh");
-        PoShenLoh(a,b,c);
+       int opc;
+       boolean salir=false;
+        
+        do{
+        System.out.println("Selecciona que deseas hacer:");
+        System.out.println("1.Eulet a la x");
+        System.out.println("2.Eulet a la -x");
+        System.out.println("3.Varianza");
+        System.out.println("4.Resolver una ecuacion cuadratica con 3 metodos diferentes");
+        System.out.println("5.Salir");
+        opc=teclado.nextInt();
+        switch(opc){
+            case 1:
+                System.out.println("calculo de e^x usando su formula igualitaria e^x=∑(n) x^j/j!");
+                System.out.println("");
+                System.out.println("introduzca X");
+                int x = teclado.nextInt();
+                System.out.println("introduzca j");
+                int j = teclado.nextInt();
+                double suma = 0;
+                for (int i = 0; i < j+1; i++) {
+                    suma = suma + Math.pow(x, i) / (factorial(i));
+                }
+                System.out.println("");
+                System.out.println("resultado: " + suma); 
+            break;
+            case 2:
+                System.out.println("calculo de e^-x usando su formula igualitaria e^-x=1/∑(n) (x^j/j!)");
+                System.out.println("");
+                System.out.println("introduzca X");
+                int y = teclado.nextInt();
+                System.out.println("introduzca j");
+                int k = teclado.nextInt();
+                double suma2 = 0;
+
+                for (int i = 0; i < k + 1; i++) {
+                    suma = suma2 + 1 / (Math.pow(y, i) / (factorial(i)));
+                }
+                System.out.println("");
+                System.out.println("resultado: " + suma2);
+            break;
+            case 3:
+                double numerosaleatorios[];
+                double summedia = 0, varianza, media,sumavar=0;
+                System.out.println("programa para sacar la varianza con su formula varianza=((x-x̅)^2+(x2-x̅)^2+...+(xn-x̅)^2)/n" );
+                System.out.println("cuantos numeros quiere agregar?");
+                int elementos = teclado.nextInt();
+                //int no;
+                numerosaleatorios = new double[elementos];
+                for (int i = 0; i < elementos; i++) {
+                    numerosaleatorios[i] =Math.random();                   //(no=tec.nextInt());
+                }
+                for (int i = 0; i < elementos; i++) {
+                    summedia += numerosaleatorios[i];
+                }
+                media = summedia / elementos;           
+                for (int i = 0; i < elementos; i++) {               
+                   sumavar+=Math.pow(numerosaleatorios[i]-media, 2);
+                }
+                varianza=sumavar/elementos;       
+                System.out.println("la varianza es: "+varianza); 
+            break;
+            case 4:
+                int a,b,c,igual;
+                System.out.println("Ingresa el valor de a(x^2)");
+                a=teclado.nextInt();
+                System.out.println("Ingresa el valor de b(x)");
+                b=teclado.nextInt();
+                System.out.println("Ingresa el valor de c(termino independiente)");
+                c=teclado.nextInt();
+                System.out.println("Todo esto es igual a:");
+                igual=teclado.nextInt();
+                c=c-igual;
+                System.out.println("Resolucion por formula general:");
+                FormulaGeneral(a,b,c);
+                System.out.println("Resolucion por el metodo de Po-Shen Loh");
+                PoShenLoh(a,b,c);
+                System.out.println("Resolucion por el metodo de Formula General a la Inversa");
+                FormulaGeneralInversa(a,b,c);
+            break;
+            case 5:
+                salir=true;
+                System.out.println("Gracias por usar el programa :)");
+            break;
+            default:
+                System.out.println("Opcion incorrecta, ingrese otra porfavor");
+            break;
+        }
+        }while(salir!=true);
+            
+        
+     
+    }
+    
+    public static boolean discriminante(int a,int b,int c){
+        double discriminante;
+        discriminante=(Math.pow(b,2))-(4*(a)*(c));
+        if(discriminante<0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
     public static void FormulaGeneral(int a,int b,int c){
         double ac,x1,x2;
         ac=(Math.pow(b,2))-(4*(a)*(c));
-        if(ac<0){
+        if(discriminante(a,b,c)==true){
             System.out.println("El problema no tiene solucion");
         }
         else{
@@ -34,8 +123,7 @@ public class EcuacionesCuadraticas {
         }
     }
     public static void PoShenLoh(int a,int b,int c){
-        double ac=(Math.pow(b,2))-(4*(a)*(c));
-        if(ac<0){
+        if(discriminante(a,b,c)==true){
             System.out.println("El problema no tiene solucion");
         }
         else{
@@ -47,6 +135,26 @@ public class EcuacionesCuadraticas {
             double x2=p-u;
             System.out.println("x1="+x1);
             System.out.println("x2="+x2);  
+        }
+        
+    }
+    public static int factorial (int numero) {
+        double factorial;
+        if (numero==0)
+        return 1;
+        else
+        return numero * factorial(numero-1);
+    }
+    
+    public static void FormulaGeneralInversa(int a,int b,int c){
+        if(discriminante(a,b,c)==true){
+            System.out.println("El problema no tiene solucion");
+        }
+        else{
+            double x1=(-2*c)/(b+(Math.sqrt(Math.pow(b,2)-(4*a*c))));
+            double x2=(-2*c)/(b-(Math.sqrt(Math.pow(b,2)-(4*a*c))));
+            System.out.println("x1="+x1);
+            System.out.println("x2="+x2);    
         }
         
     }
